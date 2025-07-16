48d8d14ef10a169cd9fd937940db1fa75cdebb5b
Gustavo Pasini <grunwald.gustavo@gmail.com>
2025-07-16 20:57:17 -0300

    ListaJavaScriptA

--- exercicios/script.js
+++ exercicios/script.js
@@ -31,17 +31,17 @@ function contarCaractere() {
 }
 
 
-function verificarParImpar(){
+function verificarParImpar() {
 
     let num1 = Number(document.getElementById("num4").value);
 
-    if(num1 % 2 == 0){
+    if (num1 % 2 == 0) {
 
         document.getElementById("resultado4").innerText = num1 + " é PAR";
 
-    }else{
+    } else {
 
-        document.getElementById("resultado4").innerText =  num1 + " é IMPAR";
+        document.getElementById("resultado4").innerText = num1 + " é IMPAR";
 
     }
 
@@ -55,20 +55,20 @@ function converterLetraMaiuscula() {
 
 }
 
-function maiorMenor(){
+function maiorMenor() {
 
     let n1 = Number(document.getElementById("num5").value);
-    let n2= Number(document.getElementById("num6").value);
+    let n2 = Number(document.getElementById("num6").value);
 
-    if (n1>n2) {
+    if (n1 > n2) {
 
-        document.getElementById("resultado6").innerText =  n1 + " é maior";
-        
+        document.getElementById("resultado6").innerText = n1 + " é maior";
+
     } else if (n2 > n1) {
 
-        document.getElementById("resultado6").innerText =  n2 + " é maior";
+        document.getElementById("resultado6").innerText = n2 + " é maior";
 
-    }else{
+    } else {
 
         document.getElementById("resultado6").innerText = "Os dois são iguais";
 
@@ -76,10 +76,10 @@ function maiorMenor(){
 
 }
 
-function gerarTabuada(){
+function gerarTabuada() {
 
     let numeroX = Number(document.getElementById("num7").value);
-    var tabuada;    
+    var tabuada;
 
     for (let indice = 1; indice <= 10; indice++) {
         if (indice == 1) {
@@ -87,16 +87,16 @@ function gerarTabuada(){
             tabuada = (numeroX + " x " + indice + " = " + (numeroX * indice) + "\n")
         } else
 
-             tabuada += (numeroX + " x " + indice + " = " + (numeroX * indice) + "\n");
+            tabuada += (numeroX + " x " + indice + " = " + (numeroX * indice) + "\n");
 
-        }
+    }
     document.getElementById("resultado7").innerText = tabuada;
 
 }
 
-function gerarSequencia(){
+function gerarSequencia() {
 
-    let sequencia = "";    
+    let sequencia = "";
 
     for (let indice2 = 1; indice2 <= 10; indice2++) {
         sequencia += indice2 + "\n";
@@ -104,21 +104,80 @@ function gerarSequencia(){
     document.getElementById("resultado8").innerText = sequencia;
 }
 
-function calcularMedia(){
+function calcularMedia() {
 
     let n1Media = Number(document.getElementById("num1Media").value);
-    let n2Media= Number(document.getElementById("num2Media").value);
-    let n3Media= Number(document.getElementById("num3Media").value);
-    let resultado = (n1Media+ n2Media +n3Media)/3;
+    let n2Media = Number(document.getElementById("num2Media").value);
+    let n3Media = Number(document.getElementById("num3Media").value);
+    let resultado1 = (n1Media + n2Media + n3Media) / 3;
 
-    if (resultado >= 6) {
+    if (resultado1 >= 6) {
 
-        document.getElementById("resultado9").innerText =  resultado + " é a sua media\n" + "Aprovado!";
-    } 
-    else{
+        document.getElementById("resultado9").innerText = resultado1 + " é a sua media\n" + "Aprovado!";
+    }
+    else {
 
-        document.getElementById("resultado9").innerText =  resultado + " é a sua media\n" + "Reprovado!";
+        document.getElementById("resultado9").innerText = resultado1 + " é a sua media\n" + "Reprovado!";
     }
 
+}
+
+
+function calcularF() {
+
+    let celcius = Number(document.getElementById("numCelcius").value);
+    let resultado2 = (celcius * 9 / 5) + 32;
+
+    document.getElementById("resultado10").innerText = "A conversão fica: " + resultado2;
+
+}
+
+function mostrarDiaSemana() {
+
+    let numDia = Number(document.getElementById("numDia").value);
+
+    if (numDia === 1) {
+        document.getElementById("resultado11").innerText = "o dia é DOMINGO!";
+    } else if (numDia === 2) {
+        document.getElementById("resultado11").innerText = "o dia é SEGUNDA!"
+    } else if (numDia === 3) {
+        document.getElementById("resultado11").innerText = "o dia é TERÇA!";
+    } else if (numDia === 4) {
+        document.getElementById("resultado11").innerText = "o dia é QUARTA!";
+    } else if (numDia === 5) {
+        document.getElementById("resultado11").innerText = "o dia é QUINTA!";
+    } else if (numDia === 6) {
+        document.getElementById("resultado11").innerText = "o dia é SEXTA!";
+    }
+    else if (numDia === 7) {
+        document.getElementById("resultado11").innerText = "o dia é SABADO!";
+    }
+    else {
+        document.getElementById("resultado11").innerText = "Numero invalido";
+    }
+
+}
+
+
+function conferirSenha() {
+
+    let senha = (document.getElementById("senha").value);
+
+    if (senha === "1234") {
+        document.getElementById("resultado12").innerText = "Acesso Permitido";
+    }
+    else {
+        document.getElementById("resultado12").innerText = "Senha incorreta";
+    }
+
+}
+function contagem() {
+
+    let contagem = "";
+
+    for (let indice = 10; indice >= 1; indice--) {
+        contagem += indice + " ";
+    }
+        document.getElementById("resultado13").innerText = contagem;
 }
 
